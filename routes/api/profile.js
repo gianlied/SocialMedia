8a7318b6fba3430b7b165f11043144fb7ed05bf3
Giancarlo Carvajal <ftapmapas@gmail.com>
2022-04-30 17:33:24 -0500

    back end

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -152,9 +152,36 @@ router.delete('/', auth, async (req, res) => {
         
         res.json({ msg: 'User deleted'});
     } catch (err) {
-        
+        console.error(err.message)
+        res.status(500).send('Server error');
     }
-})
+});
+
+//@route   Put api/profile/experience
+//@desc    add profile experience
+//@access  Private
+/*router.put('/experience', 
+[
+    auth, 
+    [
+      check('title', 'Title is required')
+      .not()
+      .isEmpty(),
+      check('team', 'Team is required')
+      .not()
+      .isEmpty(),
+      check('from', 'From date is required')
+      .not()
+      .isEmpty(),
+    ]], async (req, res) =>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        
+
+});*/
 
 
 module.exports = router;
